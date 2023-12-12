ed7826f59a918861dec08d2ec105f0a002a3c299
wilsono1 <143566000+wilsono1@users.noreply.github.com>
2023-12-12 22:46:31 +0000

    Today

--- src/assets/menu.js
+++ src/assets/menu.js
@@ -1,14 +1,14 @@
 
-  const menuButtonSelector = ".btn-toggle-menu";
-  const menuPanelSelector = ".main-nav";
-  const classToToggle = "main-nav-hidden";
-  
-  const toggleHandler = () => {
-    document.querySelector(menuPanelSelector).classList.toggle(classToToggle);
-  };
-  document
-    .querySelector(menuButtonSelector)
-    .addEventListener("click", toggleHandler);
-  const hideMenu = () => {
-    document.querySelector(menuPanelSelector).classList.add(classToToggle);
-  };
+const menuButtonSelector = ".btn-toggle-menu";
+const menuPanelSelector = ".main-nav";
+const classToToggle = "main-nav-hidden";
+
+const toggleHandler = () => {
+  document.querySelector(menuPanelSelector).classList.toggle(classToToggle);
+};
+document
+  .querySelector(menuButtonSelector)
+  .addEventListener("click", toggleHandler);
+const hideMenu = () => {
+  document.querySelector(menuPanelSelector).classList.add(classToToggle);
+};
